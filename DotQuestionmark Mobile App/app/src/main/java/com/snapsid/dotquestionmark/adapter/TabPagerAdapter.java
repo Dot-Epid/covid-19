2c49e7859be69d46ec2a1aac6c2ccfd700678cf4
Mayank Jha <jhamayank159@gmail.com>
2020-03-29 18:57:16 +0530

    webapp + androidapp

---
+++ DotQuestionmark Mobile App/app/src/main/java/com/snapsid/dotquestionmark/adapter/TabPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.snapsid.dotquestionmark.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.snapsid.dotquestionmark.fragment.LocationFragment;
+import com.snapsid.dotquestionmark.fragment.NearByFragment;
+
+public class TabPagerAdapter extends FragmentPagerAdapter {
+
+    private static final int total_pages = 2;
+
+    public TabPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NearByFragment();
+            case 1:
+                return new LocationFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return total_pages;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        switch (position) {
+            case 0:
+                return "NearBy";
+            case 1:
+                return "OTHERS";
+        }
+        return null;
+    }
+}
